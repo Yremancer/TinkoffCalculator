5ea55a03dd511a6b739f162b953ace22dca5ea90
Yremancer <chilumy@mail.ru>
2024-08-07 18:47:12 +1000

    Add Calculator Buttons

--- TinkoffCalculator/TinkoffCalculator/ViewController.swift
+++ TinkoffCalculator/TinkoffCalculator/ViewController.swift
@@ -9,6 +9,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        guard let buttonTitle = sender.titleLabel?.text else {return}
+        print(buttonTitle)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
